726b0d0ea014e61545825c3ea90fc295b791fb12
Brenda Long <brenda@studio6thave.com>
2018-07-17 06:35:21 -0500

    clean up code

--- js/dom.js
+++ js/dom.js
@@ -11,18 +11,7 @@ let showItems = (pokeData) => {
 let buildPokemon = (pokemon) => {
     //building a string to create the visual display
     //notice commas - define many with one let
-	let block = `<div class="col-md">`,
-		// wrapper = `<section class="block-wrapper" style="border: 2px solid #000000; background-color:#${pokemonColorHex}">`,
-		// title = `<h3>Name: ${pokemon.LegoName}</h3>`,
-		// years = `<div class="block-years">Manufactured ${pokemon.YearFrom} - ${pokemon.YearTo}</div>`;
-		// // image = `<div class="card-img" style="background-image: url('images/${car.image}')"></div>`,
-		// let link = pokemon.ColorstreamLinkImage;
-		// if (link){
-		// 	 block += `<a href="${link}">${wrapper + title + years}</section></a>`;
-		// }else{
-		// 	block += `${wrapper + title + years}</section>`;
-        // }
-        
+	let block = `<div class="col-md">`, 
         card = `<div class="card" style="width: 18rem;">`,
         img = `<img class="card-img-top" src="${pokemon.ThumbnailImage}" alt="${pokemon.name}">`,
         content = `<div class="card-body">
